a31dfc2c8493243a186bf09be2536c0acace90b4
sigoden <sigoden@gmail.com>
2022-03-21 22:53:50 +0800

    feat: customize with ini file (#15)
    * feat: customize with ini file
    
    * implement

--- README.md
+++ README.md
@@ -2,6 +2,20 @@
 
 Switch windows of **same** app with ``` alt + ` ``` on windows pc.
 
-- 200k single file executable downloaded from [Github Release](https://github.com/sigoden/windows-switcher/releases).
+- 250k single file executable downloaded from [Github Release](https://github.com/sigoden/windows-switcher/releases).
 - No installation required, just run it.
 - Easily enable running on startup with trayicon.
+
+## Customize
+
+Create a `windows-switcher.ini` file in the same directory as `windows-switcher.exe`.
+
+The default configuration is:
+
+````ini
+trayicon = yes
+hotkey = alt+`
+````
+
+1. Modify `trayicon = no` to hide the trayicon.
+2. Modify `hotkey` to custom hotkey.
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
 #![windows_subsystem = "windows"]
 
-use windows_switcher::start_app;
+use ini::Ini;
+use windows_switcher::{start_app, Config};
 
 fn main() {
-    start_app();
+    let config = load_config().unwrap_or_default();
+    start_app(&config);
+}
+
+fn load_config() -> Option<Config> {
+    let mut ini_file = std::env::current_exe().ok()?;
+    ini_file.pop();
+    ini_file.push("windows-switcher.ini");
+    let conf = Ini::load_from_file(ini_file).ok()?;
+    let sec = conf.section(None::<String>)?;
+    let default_config = Config::default();
+    let trayicon = sec
+        .get("trayicon")
+        .and_then(Config::to_bool)
+        .unwrap_or(default_config.trayicon);
+    let hotkey = sec
+        .get("hotkey")
+        .and_then(Config::to_hotkey)
+        .unwrap_or(default_config.hotkey);
+    Some(Config { trayicon, hotkey })
 }

c73300464c6cf51c36b9700e46a53a1197b77639
sigoden <sigoden@gmail.com>
2023-02-10 09:03:32 +0800

    feat: adjust the configuration file
    BREAKING CHANGE: configuration structure changed

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![windows_subsystem = "windows"]
 
 use ini::Ini;
-use windows_switcher::{start_app, Config, HotKeyConfig};
+use windows_switcher::{start_app, Config};
 
 fn main() {
     let config = load_config().unwrap_or_default();
@@ -13,30 +13,5 @@ fn load_config() -> Option<Config> {
     ini_file.pop();
     ini_file.push("windows-switcher.ini");
     let conf = Ini::load_from_file(ini_file).ok()?;
-    let section = conf.section(None::<String>)?;
-    let default_config = Config::default();
-    let trayicon = section
-        .get("trayicon")
-        .and_then(Config::to_bool)
-        .unwrap_or(default_config.trayicon);
-    let hotkey = section
-        .get("hotkey")
-        .and_then(HotKeyConfig::parse)
-        .unwrap_or(default_config.hotkey);
-    let blacklist = section
-        .get("blacklist")
-        .map(|v| {
-            let v = v.trim();
-            if v.is_empty() {
-                String::new()
-            } else {
-                format!(",{}", v.trim().to_lowercase())
-            }
-        })
-        .unwrap_or_default();
-    Some(Config {
-        trayicon,
-        hotkey,
-        blacklist,
-    })
+    Some(Config::load(&conf))
 }

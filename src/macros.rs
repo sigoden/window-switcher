be44bb59e03a3e6baa7678900b45fc30144bee28
sigoden <sigoden@gmail.com>
2023-02-18 08:00:16 +0800

    refactor: improve code quanity (#27)

--- src/macros.rs
+++ src/macros.rs
@@ -1,37 +1,46 @@
 use std::ffi::CString;
+use std::fmt::Display;
 
-use windows::Win32::Foundation::PSTR;
+use windows::core::PCSTR;
 use windows::Win32::System::Diagnostics::Debug::OutputDebugStringA;
-pub fn output_debug<T: AsRef<str>>(text: T) {
-    let data = CString::new(text.as_ref()).unwrap();
-    unsafe { OutputDebugStringA(PSTR(data.as_ptr() as *const u8)) };
+use windows::Win32::UI::WindowsAndMessaging::{MessageBoxA, MB_ICONERROR, MB_OK};
+
+pub fn output_debug<T: Display>(text: T) {
+    let data = CString::new(text.to_string()).unwrap();
+    unsafe { OutputDebugStringA(PCSTR(data.as_ptr() as *const u8)) };
+}
+
+pub fn message_box<T: Display>(text: T) {
+    let lp_text = CString::new(text.to_string()).unwrap();
+    let lp_caption = CString::new("Error").unwrap();
+    unsafe {
+        MessageBoxA(
+            None,
+            PCSTR(lp_text.as_ptr() as *const u8),
+            PCSTR(lp_caption.as_ptr() as *const u8),
+            MB_OK | MB_ICONERROR,
+        )
+    };
 }
 
 #[macro_export]
-macro_rules! log_error {
-    ($msg:literal $(,)?) => {
-        $crate::macros::output_debug($msg);
-    };
-    ($err:expr $(,)?) => {
-        $crate::macros::output_debug($err);
-    };
-    ($fmt:expr, $($arg:tt)*) => {
-        $crate::macros::output_debug(&format!($fmt, $($arg)*));
+macro_rules! error {
+	($($arg:tt)*) => {
+        $crate::macros::output_debug(format!($($arg)*))
     };
 }
 
 #[macro_export]
-macro_rules! log_info {
-    ($msg:literal $(,)?) => {
-		#[cfg(debug_assertions)]
-        $crate::macros::output_debug($msg);
+macro_rules! debug {
+	($($arg:tt)*) => {
+        #[cfg(debug_assertions)]
+        $crate::macros::output_debug(format!($($arg)*))
     };
-    ($err:expr $(,)?) => {
-		#[cfg(debug_assertions)]
-        $crate::macros::output_debug($err);
-    };
-    ($fmt:expr, $($arg:tt)*) => {
-		#[cfg(debug_assertions)]
-        $crate::macros::output_debug(&format!($fmt, $($arg)*));
+}
+
+#[macro_export]
+macro_rules! alert {
+    ($($arg:tt)*) => {
+        $crate::macros::message_box(format!($($arg)*))
     };
 }
--- src/trayicon.rs
+++ src/trayicon.rs
@@ -1,22 +1,21 @@
-use crate::app::{MENU_CMD_EXIT, MENU_CMD_STARTUP, NAME, WM_USER_TRAYICON};
-use crate::Win32Result;
+use crate::app::{IDM_EXIT, IDM_STARTUP, NAME, WM_USER_TRAYICON};
 
 use anyhow::{anyhow, Result};
-use std::{mem::size_of, ptr};
-use wchar::{wchar_t, wchz};
-use windows::Win32::Foundation::{HWND, POINT, PWSTR};
+use windows::core::PCWSTR;
+use windows::w;
+use windows::Win32::Foundation::{HWND, POINT};
 use windows::Win32::UI::Shell::{
     Shell_NotifyIconW, NIF_ICON, NIF_MESSAGE, NIF_TIP, NIM_ADD, NIM_DELETE, NOTIFYICONDATAW,
 };
 use windows::Win32::UI::WindowsAndMessaging::{
-    AppendMenuW, CreateIconFromResourceEx, CreatePopupMenu, DestroyMenu, GetCursorPos,
-    LookupIconIdFromDirectoryEx, SetForegroundWindow, TrackPopupMenu, HICON, HMENU,
-    LR_DEFAULTCOLOR, MF_CHECKED, MF_STRING, MF_UNCHECKED, TPM_BOTTOMALIGN, TPM_LEFTALIGN,
+    AppendMenuW, CreateIconFromResourceEx, CreatePopupMenu, GetCursorPos,
+    LookupIconIdFromDirectoryEx, SetForegroundWindow, TrackPopupMenu, HMENU, LR_DEFAULTCOLOR,
+    MF_CHECKED, MF_STRING, MF_UNCHECKED, TPM_BOTTOMALIGN, TPM_LEFTALIGN,
 };
 
-const TRAYICON_ICON_BUFFER: &[u8] = include_bytes!("../assets/icon.ico");
-const TEXT_STARTUP: &[wchar_t] = wchz!("Startup");
-const TEXT_EXIT: &[wchar_t] = wchz!("Exit");
+const ICON_BYTES: &[u8] = include_bytes!("../assets/icon.ico");
+const TEXT_STARTUP: PCWSTR = w!("Startup");
+const TEXT_EXIT: PCWSTR = w!("Exit");
 
 pub struct TrayIcon {
     data: NOTIFYICONDATAW,
@@ -24,79 +23,75 @@ pub struct TrayIcon {
 
 impl TrayIcon {
     pub fn create() -> Self {
-        let data = Self::gen_data();
+        let data = Self::create_nid();
         Self { data }
     }
-    pub fn add(&mut self, hwnd: HWND) -> Result<()> {
+
+    pub fn register(&mut self, hwnd: HWND) -> Result<()> {
         self.data.hWnd = hwnd;
         unsafe { Shell_NotifyIconW(NIM_ADD, &self.data) }
             .ok()
             .map_err(|e| anyhow!("Fail to add trayicon, {}", e))
     }
-    pub fn popup(&mut self, startup: bool) -> Result<()> {
-        let hwnd = self.hwnd();
-        let mut point = POINT::default();
+
+    pub fn show(&mut self, startup: bool) -> Result<()> {
+        let hwnd = self.data.hWnd;
+        let mut cursor = POINT::default();
         unsafe {
             SetForegroundWindow(hwnd)
                 .ok()
                 .map_err(|e| anyhow!("Fail to set foreground window, {}", e))?;
-            GetCursorPos(&mut point)
+            GetCursorPos(&mut cursor)
                 .ok()
                 .map_err(|e| anyhow!("Fail to get cursor pos, {}", e))?;
-            let menu = self
+            let hmenu = self
                 .create_menu(startup)
                 .map_err(|e| anyhow!("Fail to create menu, {}", e))?;
-
             TrackPopupMenu(
-                menu.hmenu,
+                hmenu,
                 TPM_LEFTALIGN | TPM_BOTTOMALIGN,
-                point.x,
-                point.y,
+                cursor.x,
+                cursor.y,
                 0,
                 hwnd,
-                ptr::null_mut(),
+                None,
             )
             .ok()
             .map_err(|e| anyhow!("Fail to show popup menu, {}", e))?
         };
         Ok(())
     }
-    fn hwnd(&self) -> HWND {
-        self.data.hWnd
-    }
-    fn gen_data() -> NOTIFYICONDATAW {
-        let icon = unsafe { convert_icon(TRAYICON_ICON_BUFFER) };
-        let mut sz_tip = [0; 128];
-        sz_tip[..NAME.len()].copy_from_slice(NAME);
+
+    fn create_nid() -> NOTIFYICONDATAW {
+        let offset = unsafe {
+            LookupIconIdFromDirectoryEx(ICON_BYTES.as_ptr(), true, 0, 0, LR_DEFAULTCOLOR)
+        };
+        let icon_data = &ICON_BYTES[offset as usize..];
+        let hicon =
+            unsafe { CreateIconFromResourceEx(icon_data, true, 0x30000, 0, 0, LR_DEFAULTCOLOR) }
+                .expect("Failed to load icon resource");
+        let mut tooltip: Vec<u16> = unsafe { NAME.as_wide() }.to_vec();
+        tooltip.resize(128, 0);
+        tooltip.pop();
+        tooltip.push(0);
+        let tooltip: [u16; 128] = tooltip.try_into().unwrap();
         NOTIFYICONDATAW {
-            cbSize: size_of::<NOTIFYICONDATAW>() as u32,
             uID: WM_USER_TRAYICON,
             uFlags: NIF_ICON | NIF_MESSAGE | NIF_TIP,
             uCallbackMessage: WM_USER_TRAYICON,
-            hIcon: icon,
-            szTip: sz_tip,
+            hIcon: hicon,
+            szTip: tooltip,
             ..Default::default()
         }
     }
-    fn create_menu(&mut self, startup: bool) -> Win32Result<WrapHMenu> {
+
+    fn create_menu(&mut self, startup: bool) -> Result<HMENU> {
         let startup_flags = if startup { MF_CHECKED } else { MF_UNCHECKED };
         unsafe {
-            let hmenu = CreatePopupMenu();
-            AppendMenuW(
-                hmenu,
-                startup_flags,
-                MENU_CMD_STARTUP as usize,
-                PWSTR(TEXT_STARTUP.as_ptr()),
-            )
-            .ok()?;
-            AppendMenuW(
-                hmenu,
-                MF_STRING,
-                MENU_CMD_EXIT as usize,
-                PWSTR(TEXT_EXIT.as_ptr()),
-            )
-            .ok()?;
-            Ok(WrapHMenu { hmenu })
+            let hmenu = CreatePopupMenu().map_err(|err| anyhow!("Failed to create menu, {err}"))?;
+            AppendMenuW(hmenu, startup_flags, IDM_STARTUP as usize, TEXT_STARTUP).ok()?;
+            AppendMenuW(hmenu, MF_STRING, IDM_EXIT as usize, TEXT_EXIT).ok()?;
+            Ok(hmenu)
         }
     }
 }
@@ -105,28 +100,4 @@ impl Drop for TrayIcon {
     fn drop(&mut self) {
         unsafe { Shell_NotifyIconW(NIM_DELETE, &self.data) };
     }
-}
-
-struct WrapHMenu {
-    hmenu: HMENU,
-}
-
-impl Drop for WrapHMenu {
-    fn drop(&mut self) {
-        unsafe { DestroyMenu(self.hmenu) };
-    }
-}
-
-unsafe fn convert_icon(buffer: &[u8]) -> HICON {
-    let offset = { LookupIconIdFromDirectoryEx(buffer.as_ptr(), true, 0, 0, LR_DEFAULTCOLOR) };
-    let icon_data = &buffer[offset as usize..];
-    CreateIconFromResourceEx(
-        icon_data.as_ptr(),
-        icon_data.len() as u32,
-        true,
-        0x30000,
-        0,
-        0,
-        LR_DEFAULTCOLOR,
-    )
 }

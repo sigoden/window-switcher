257ee5f8be3394eda48094208643bd4b72ffb776
sigoden <sigoden@gmail.com>
2023-03-28 22:16:31 +0800

    chore: release v0.12.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
+pub mod utils;
+#[macro_use]
+pub mod macros;
+#[macro_use]
+extern crate log;
+
 mod app;
 mod config;
 mod foregound;
 mod keyboard;
 mod startup;
 mod trayicon;
-
-pub mod utils;
-#[macro_use]
-pub mod macros;
-#[macro_use]
-extern crate log;
 
 pub use crate::app::start;
 pub use crate::config::Config;

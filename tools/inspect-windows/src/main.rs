2f83766a5e2400d37c4a6e058d2e193d32d59c2d
sigoden <sigoden@gmail.com>
2023-11-05 11:01:47 +0800

    feat: support windows-arm (#84)
    * feat: support windows arm
    
    * add windows-arm to ci
    
    * add new target: i686-pc-windows-msvc
    
    * set version to 1.4.0-rc1
    
    * update inspect-windows
    
    * remove target(aarch64-pc-windows-msvc) from ci
    
    * update inspect-windows
    
    * update release ci

--- tools/inspect-windows/src/main.rs
+++ tools/inspect-windows/src/main.rs
@@ -1,13 +1,55 @@
-use anyhow::{anyhow, Result};
+use anyhow::{Context, Result};
 use window_switcher::utils::*;
 
 use windows::Win32::Foundation::{BOOL, HWND, LPARAM};
 use windows::Win32::UI::WindowsAndMessaging::{EnumWindows, GetWindow, GW_OWNER};
 
 fn main() -> Result<()> {
+    let output = collect_windows_info()?
+        .iter()
+        .map(|v| v.stringify())
+        .collect::<Vec<String>>()
+        .join("\n");
+    println!("{output}");
+    Ok(())
+}
+
+#[derive(Debug)]
+struct WindowInfo {
+    hwnd: HWND,
+    title: String,
+    owner_hwnd: HWND,
+    owner_title: String,
+    size: (usize, usize),
+    is_visible: bool,
+    is_cloaked: bool,
+    is_iconic: bool,
+    is_topmost: bool,
+}
+
+impl WindowInfo {
+    pub fn stringify(&self) -> String {
+        let size = format!("{}x{}", self.size.0, self.size.1);
+        format!(
+            "visible:{}cloacked{}iconic{}topmost:{} {:>10} {:>10}:{} {}:{}",
+            pretty_bool(self.is_visible),
+            pretty_bool(self.is_cloaked),
+            pretty_bool(self.is_iconic),
+            pretty_bool(self.is_topmost),
+            size,
+            self.hwnd.0,
+            self.title,
+            self.owner_hwnd.0,
+            self.owner_title
+        )
+    }
+}
+
+fn collect_windows_info() -> anyhow::Result<Vec<WindowInfo>> {
     let mut hwnds: Vec<HWND> = Default::default();
     unsafe { EnumWindows(Some(enum_window), LPARAM(&mut hwnds as *mut _ as isize)) }
-        .map_err(|e| anyhow!("Fail to get windows {}", e))?;
+        .with_context(|| "Fail to enum windows".to_string())?;
+    let mut output = vec![];
     for hwnd in hwnds {
         let title = get_window_title(hwnd);
         let is_cloaked = is_cloaked_window(hwnd);
@@ -21,21 +63,20 @@ fn main() -> Result<()> {
         } else {
             "".into()
         };
-        let size = format!("{width}x{height}");
-        println!(
-            "visible:{}cloacked{}iconic{}topmost:{} {:>10} {:>10}:{} {}:{}",
-            pretty_bool(is_visible),
-            pretty_bool(is_cloaked),
-            pretty_bool(is_iconic),
-            pretty_bool(is_topmost),
-            size,
-            hwnd.0,
+        let window_info = WindowInfo {
+            hwnd,
             title,
-            owner_hwnd.0,
-            owner_title
-        );
+            owner_hwnd,
+            owner_title,
+            size: (width as usize, height as usize),
+            is_visible,
+            is_cloaked,
+            is_iconic,
+            is_topmost,
+        };
+        output.push(window_info);
     }
-    Ok(())
+    Ok(output)
 }
 
 fn pretty_bool(value: bool) -> String {
@@ -50,4 +91,9 @@ extern "system" fn enum_window(hwnd: HWND, lparam: LPARAM) -> BOOL {
     let windows: &mut Vec<HWND> = unsafe { &mut *(lparam.0 as *mut _) };
     windows.push(hwnd);
     BOOL(1)
+}
+
+#[test]
+fn test_collect_windows_info() {
+    collect_windows_info().unwrap();
 }

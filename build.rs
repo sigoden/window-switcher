879fc7d34b9e080782e35c9739dfdb77b18ae400
sigoden <sigoden@gmail.com>
2024-11-15 08:13:29 +0800

    chore: update deps

--- build.rs
+++ build.rs
@@ -1,3 +1,5 @@
 fn main() {
-    embed_resource::compile("assets/app.rc", embed_resource::NONE);
+    embed_resource::compile("assets/app.rc", embed_resource::NONE)
+        .manifest_optional()
+        .expect("Failed to compile resource file");
 }
